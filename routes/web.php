eb24dfe2b3e9b92bc11c8f119e7519c25fddbb8e
pwtansg <pwtan.edu@gmail.com>
2023-04-18 21:28:40 +0800

    Add Admin Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\VisitorController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
@@ -23,12 +24,13 @@ Route::get('/visitor/register', [VisitorController::class,'create']);
 Route::get('/admin/login', [LoginController::class, 'index'])->name('login');
 Route::post('/admin/login', [LoginController::class, 'store']);
 Route::post('/admin/logout', [LogoutController::class, 'store'])->name('logout');
-Route::get('/register', [RegisterController::class, 'index'])->name('register');
-Route::post('/register', [RegisterController::class, 'store']);
+// Route::get('/register', [RegisterController::class, 'index'])->name('register');
+// Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/admin/visitor', [VisitorController::class, 'index'])->middleware('auth')->name('visitor');
 Route::get('/admin/visitor/{id}', [VisitorController::class,'show'])->middleware('auth');
-Route::put('/admin/visitor/{id}', [VisitorController::class,'update'])->middleware('auth');;
+Route::put('/admin/visitor/{id}', [VisitorController::class,'update'])->middleware('auth');
+Route::get('/admin/dashboard', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard');
 
 // Route::get('/home', function () {
 //     return view('home');
